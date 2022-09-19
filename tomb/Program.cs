504c964c9e3e941edc11c127515568c29d87201d
Forry <ebarni04@gmail.com>
2022-09-19 08:31:34 +0200

    gamar

--- tomb/Program.cs
+++ tomb/Program.cs
@@ -11,26 +11,45 @@ namespace tomb
         static void Main(string[] args)
         {
 
-            int[] tomb = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int i = 0;
-            while (i < 10)
+            //int[] tomb = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] tomb = new int[10];
+            tomb[5] = 100;
+
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine($"{i + 1}. elem: {tomb[i]}");
+            //    i++;
+            //}
+
+
+
+            //Console.WriteLine(tomb[0]);
+            //Console.WriteLine(tomb[9]);
+            //Console.WriteLine(tomb.Length - 1);
+
+
+            //for (int i = tomb.Length-1; i >= 0; i--)
+            //{
+            //    Console.WriteLine($"{i+1}. elem: {tomb[i]}");
+            //    Console.WriteLine($"{i+1}. elem: {tomb[i]}");
+            //}
+
+            Random veletlen = new Random();
+
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"{i + 1}. elem: {tomb[i]}");
-                i++;
+                tomb[i] = veletlen.Next(100);
+
             }
 
 
-            /*
-            Console.WriteLine(tomb[0]);
-            Console.WriteLine(tomb[9]);
-            Console.WriteLine(tomb.Length-1);
-            */
-
-            /*for (int i = tomb.Length-1; i >= 0; i--)
+            foreach (int szam in tomb)
             {
-                Console.WriteLine($"{i+1}. elem: {tomb[i]}");
-                Console.WriteLine($"{i+1}. elem: {tomb[i]}");
-            }*/
+                Console.WriteLine(szam);
+            }
+
+
 
             Console.ReadKey();
 
